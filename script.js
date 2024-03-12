ce06d13965f573044f4c0c084d583ee6aeefa7f9
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-03-12 21:25:31 +0200

    Included the regex to match the requirements

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
     // Check if input starts with 'pet_' and followed by alphanumeric characters
-    if (input) {
-        input = (^ 'pet_[a-zA-Z0-9]$);
-            let result = "Valid Syntax "
+    if (/^pet_\[a-zA-Z0-9]+$/.test(input)) {
+        result = "Valid Syntax"
     } else {
-        let result = "Invalid Syntax"
+        result = "Invalid Syntax"
     }
     document.getElementById('result').innerText = result;
 }
