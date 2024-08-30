61426b9f12c5bfe6c19c29cd80eab2fd1bc7f6fd
Steven <stevenaknobel@gmail.com>
2024-08-30 12:48:34 +0200

    add regular expression with \w shorthand to validate the expression

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ function validateSyntax() {
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
 
+    //create regular expression with \w shorthand for alphanumeric
+    //^pet_ must match this pattern exactly
+    //+ at the end represents additional occurances of the previous element, in this case \w which is the alphanumeric
+    const regex = /^pet_\w+$/;
+
+
+    //validate using regular expression test
+    if (regex.test(input)){
+        result = "Valid Syntax"
+    } else {
+        result = "Invalid Syntax"
+    }
+
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
